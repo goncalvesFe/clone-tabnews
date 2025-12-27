ba9c2ab696297ab1498f003e54c0be7a3194eaf6
Felipe Gonçalves <62034737+goncalvesFe@users.noreply.github.com>
2025-12-27 03:30:39 +0000

    fix /migrations endpoint connection bug

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,27 +3,41 @@ import { join } from "node:path";
 import database from "infra/database";
 
 async function migrations(request, response) {
-  const dbClient = await database.getNewClient();
-  const isPostRequest = request.method === "POST" ? true : false;
-  const migrationsOptions = {
-    dbClient,
-    dryRun: true,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  };
+  const { method } = request;
+  const allowedMethods = ["GET", "POST"];
+  if (!allowedMethods.includes(method))
+    return response.status(405).json({
+      error: `Method "${method}" not allowed`,
+    });
 
-  if (isPostRequest) migrationsOptions.dryRun = false;
+  let dbClient;
 
-  const migrations = await migrationRunner(migrationsOptions);
+  try {
+    dbClient = await database.getNewClient();
+    const isPostRequest = method === "POST" ? true : false;
+    const migrationsOptions = {
+      dbClient,
+      dryRun: true,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    };
 
-  await dbClient.end();
+    if (isPostRequest) migrationsOptions.dryRun = false;
 
-  if (migrations.length > 0 && isPostRequest)
-    return response.status(201).json(migrations);
+    const migrations = await migrationRunner(migrationsOptions);
 
-  return response.status(200).json(migrations);
+    if (migrations.length > 0 && isPostRequest)
+      return response.status(201).json(migrations);
+
+    return response.status(200).json(migrations);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
+    await dbClient.end();
+  }
 }
 
 export default migrations;
